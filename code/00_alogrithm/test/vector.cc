834ecc872ba00e97e8efdf85e1327bf731ef9ac6
wsl <hbag666@163.com>
2021-03-19 10:11:45 +0800

    3/19

--- code/00_alogrithm/test/vector.cc
+++ code/00_alogrithm/test/vector.cc
@@ -1,10 +1,12 @@
 #include <vector>
 #include <iostream>
 #include <algorithm>
+#include <string>
 using namespace std;
 
 int main()
 {
     vector<int> v = {3, 2, 1};
     reverse(v.begin(), v.end());
+    to_string(1);
 }
