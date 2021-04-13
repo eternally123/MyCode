d7fa24f55e0ded175fce5d727e387e1af854b683
wsl <hbag666@163.com>
2021-04-13 18:00:38 +0800

    4/13

--- code/01_language/C++11/atomic/main.cc
+++ code/01_language/C++11/atomic/main.cc
@@ -5,8 +5,8 @@ using namespace std;
 
 int main()
 {
-    atomic<int> total;
-    atomic_store(&total, 0);
+    atomic<int> total{0}; // 初始化不能用=赋值
+    total = 1;            // 相当于atomic_store(&total, 0);
     total++;
     cout << total << endl;
 
